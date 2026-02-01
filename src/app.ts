29d339b449076f14fd7a0009b849c8473189808e
khodakivskiy <andriyhodak2007@gmail.com>
2026-02-01 12:58:18 +0200

    implement optional rate liming

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,14 @@ import express from 'express';
 import healthRoutes from './routes/health';
 import notificationRoutes from './routes/notifications';
 import errorHandler from './middleware/errorHandler';
+import rateLimitHandler from './middleware/rateLimitHandler';
+import config from './config/env';
 
 const app = express();
+
 app.use(express.json());
+if (config.rateLimiting.rateLimitEnabled) app.use(rateLimitHandler);
+
 app.use('/api', healthRoutes);
 app.use('/api/notifications', notificationRoutes);
 

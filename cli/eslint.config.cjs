e15134eb1a04a6b44abef9682d7fae5de3b2b713
Andrii Khodakivskyi <andriyhodak2007@gmail.com>
2026-05-09 18:48:03 +0300

    refactor: update ESLint configuration to use separate TypeScript config file (#37)

--- cli/eslint.config.cjs
+++ cli/eslint.config.cjs
@@ -7,7 +7,7 @@ module.exports = [
     languageOptions: {
       parser: tsParser,
       parserOptions: {
-        project: './tsconfig.json',
+        project: './tsconfig.eslint.json',
         tsconfigRootDir: __dirname,
         ecmaVersion: 2020,
         sourceType: 'module',
